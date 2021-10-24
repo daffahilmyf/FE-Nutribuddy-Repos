58b7bf71c08e942f6be947ad3d50cf15c940e3d7
Bagasarfiansyah007 <55185733+Bagasarfiansyah007@users.noreply.github.com>
2021-10-24 09:47:46 +0700

    update - New File - sunday, 24 Oktober 2021
    menambahkan file css dan file login dan singup baru.

--- JS/dashboardhHome-js.js
+++ JS/dashboardhHome-js.js
@@ -4,10 +4,15 @@ function collapse(elem){
     logoTitle = document.getElementById("logo").querySelector("span:nth-child(2)");
     imgLogo = document.getElementById("logo")
     listTitle = document.getElementById("content");
+    homeDashboard = document.getElementById("home");
+    bodyDashboard = document.getElementById("marginright");
+
 
     collapseComponent.classList.toggle('active');
     logoTitle.classList.toggle('active');
     elem.classList.toggle('active');
     listTitle.classList.toggle('active');
     imgLogo.classList.toggle("active");
+    homeDashboard.classList.toggle("active");
+    bodyDashboard.classList.toggle("active");
 }
